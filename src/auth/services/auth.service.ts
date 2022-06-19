8c77174ae227e73531d3d6b2484fd91db9cc3a07
Dani <danimm1989@gmail.com>
2022-06-19 15:58:35 +0200

    using passport strategies

---
+++ src/auth/services/auth.service.ts
@@ -0,0 +1,22 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { UsersService } from '../../users/services/users.service';
+import * as bycrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(@Inject(UsersService) private usersService: UsersService) {}
+
+  /**
+   * Validate a user using email and password
+   * @param email
+   * @param password
+   */
+  async validateUser(email: string, password: string) {
+    const user = await this.usersService.findByEmail(email);
+    const matchedPassword = await bycrypt.compare(password, user.password);
+    if (user && matchedPassword) {
+      return user;
+    }
+    return null;
+  }
+}
